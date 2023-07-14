9fc6732173dc7519557ffcd720a76384d862d239
v-leonel <vitorleonell123@gmail.com>
2023-07-14 07:24:23 -0300

    Game repository, service, controller

--- src/main/java/com/victorleonel/dslist/services/GameService.java
+++ src/main/java/com/victorleonel/dslist/services/GameService.java
@@ -1,5 +1,24 @@
 package com.victorleonel.dslist.services;
 
-public class GameService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorleonel.dslist.dto.GameMinDTO;
+import com.victorleonel.dslist.entities.Game;
+import com.victorleonel.dslist.repositories.GameRepository;
+
+@Service
+public abstract class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public abstract List<GameMinDTO> findAll();{
+		List<GameMinDTO> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+		
+			
 }
